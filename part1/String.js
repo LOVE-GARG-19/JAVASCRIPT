220f91e63a8d2b17e10a3aae8d6287dd13a4db57
LoveGarg <lovegarg2800@gmail.com>
2024-06-28 12:16:57 +0530

    JAVASCRIPT

---
+++ part1/String.js
@@ -0,0 +1,18 @@
+// String indexing 
+
+let firstName = "love";
+
+//  l o v e 
+//  0 1 2 3 
+
+console.log(firstName[0]);
+
+
+// length of string 
+// firstName.length 
+
+console.log(firstName.length);
+
+console.log(firstName[firstName.length-2]);
+
+// last Index : length - 1 
---
+++ part1/String_method.js
@@ -0,0 +1,25 @@
+// trim() 
+// toUpperCase()
+// toLowerCase()
+// slice()
+
+// let firstName = "    loVe   ";
+
+// console.log(firstName.length);
+// firstName = firstName.trim(); // love
+// console.log(firstName);
+// console.log(firstName.length);
+// firstName=firstName.toLowerCase();
+// firstName=firstName.toUpperCase();
+// console.log(firstName);
+
+
+//slice method
+// start index 
+// end index // not include
+
+let firstName="lovegarg"
+// l o v e g a r g
+// 0 1 2 3 4 5 6 7
+let newString = firstName.slice(4,firstName.length); //love
+console.log(newString);
